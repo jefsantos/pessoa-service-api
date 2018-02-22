d76bf9e61e824e830e098d58cdf4d4d0c894cb34
Jeferson Alves dos Santos <jsantos@apoioprodesp.sp.gov.br>
2018-02-22 17:20:01 -0300

    APRENDENDO

--- src/main/java/com/calasans/PessoaServiceApplication.java
+++ src/main/java/com/calasans/PessoaServiceApplication.java
@@ -5,6 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class PessoaServiceApplication {
+	
+	
 
 	public static void main(String[] args) {
 		SpringApplication.run(PessoaServiceApplication.class, args);
